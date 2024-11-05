ee96e3b510c76b601e3779a9fd75b1a43215811c
ngweileen <ngweileen@yahoo.co.uk>
2024-11-05 23:00:04 +0800

    first commit

---
+++ lib/main.rb
@@ -0,0 +1,48 @@
+module Linkedlist
+    class LinkedList
+        def initialize
+        end
+
+        def append(value)
+        end
+
+        def prepend(value)
+        end
+
+        def size
+        end
+
+        def head
+        end
+
+        def tail
+        end
+
+        def at(index)
+        end
+
+        def pop
+        end
+
+        def contains?(value)
+        end
+
+        def find(value)
+        end
+
+        def to_s
+        end
+
+        #bonus
+        def insert_at(value, index)
+        end
+
+        def remove_at(index)
+        end
+    end
+
+    class Node
+        def initialize
+        end
+    end
+end
